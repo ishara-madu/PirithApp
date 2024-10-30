bba380d8007b2bc5e4ae706aa604936012a3a7c2
Ishara <isharamadushankab@gmail.com>
2024-10-30 20:02:11 +0530

    update Menu.tsx

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,6 +1,6 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity,SafeAreaView } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+// import { SafeAreaView } from 'react-native-safe-area-context'
 import Home from '../../assets/svg/Home'
 import Playlist from '../../assets/svg/Playlist'
 import Settings from '../../assets/svg/Settings'
@@ -10,7 +10,7 @@ import Rate from '../../assets/svg/Rate'
 import { useGlobalContext } from '../components/Hooks/GlobalContext'
 
 const Menu = () => {
-  const {showMenu,setShowMenu} = useGlobalContext();
+  const {showMenu,setShowMenu,setShowPlaylist} = useGlobalContext();
 
   return (
     <SafeAreaView className='bg-black flex w-full h-full absolute'>
@@ -23,11 +23,11 @@ const Menu = () => {
       </View>
       <View className='w-full flex-1 bg-slate-700 items-center rounded-t-3xl pt-10'>
         <View className='w-[70%] flex-1'>
-          <TouchableOpacity className='h-14 active:bg-[#0000002f] rounded-xl w-full flex flex-row items-center pl-5'>
+          <TouchableOpacity onPress={()=>{setShowMenu(false);setShowPlaylist(false)}} className='h-14 active:bg-[#0000002f] rounded-xl w-full flex flex-row items-center pl-5'>
             <Home />
             <Text className='text-white text-lg ml-8'>Home</Text>
           </TouchableOpacity>
-          <TouchableOpacity className='h-14 active:bg-[#0000002f] rounded-xl w-full flex flex-row items-center pl-5'>
+          <TouchableOpacity onPress={()=>{setShowMenu(false)}} className='h-14 active:bg-[#0000002f] rounded-xl w-full flex flex-row items-center pl-5'>
             <Playlist />
             <Text className='text-white text-lg ml-8'>Playlist</Text>
           </TouchableOpacity>
