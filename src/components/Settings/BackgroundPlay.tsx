b0d615421f78e107c3fb5bfef5129132cb2f90d6
Ishara <isharamadushankab@gmail.com>
2024-10-31 20:12:16 +0530

    update BackgroundPlayIcon.tsx and BackgroundPlay.tsx

--- src/components/Settings/BackgroundPlay.tsx
+++ src/components/Settings/BackgroundPlay.tsx
@@ -5,6 +5,7 @@ import DownArrow from '../../../assets/svg/DownArrow';
 import Player from '../../../assets/svg/Player';
 import ThemeIcon from '../../../assets/svg/ThemeIcon';
 import BackgroundPlayIcon from '../../../assets/svg/BackgroundPlayIcon';
+import { darkStyles, lightStyles, useGlobalContext } from '../Hooks/GlobalContext';
 
 type SleepProps = {
     handleBackgroundPlayButton?: any;
@@ -15,6 +16,8 @@ const BackgroundPlay = ({ handleBackgroundPlayButton, ...props }: SleepProps) =>
     const [showBackgroundPlayOptions, setShowBackgroundPlayOptions] = useState(false);
     const [selectedBackgroundPlay, setSelectedBackgroundPlay] = useState("Light");
 
+    const { theme } = useGlobalContext();
+    const currentStyles = theme === 'Light' ? lightStyles : darkStyles;
 
     const bacgroundOptions = ["off", "on",]
 
@@ -29,11 +32,11 @@ const BackgroundPlay = ({ handleBackgroundPlayButton, ...props }: SleepProps) =>
     return (
         <View className='flex flex-row justify-between items-center w-full h-full'>
             <View className='flex flex-row items-center gap-x-3 h-full'>
-                <BackgroundPlayIcon />
-                <Text className='text-white text-md font-semibold'>Background Play</Text>
+                <BackgroundPlayIcon fill={currentStyles.svg_1}/>
+                <Text className={`${currentStyles.tx_1} text-md font-semibold`}>Background Play</Text>
             </View>
             <View className='flex flex-row items-center h-full gap-1 relative'>
-                <TouchableOpacity className='absolute right-2 flex h-auto items-center justify-center px-2 w-28 flex-row rounded-md bg-[#b7b7b7d2]'
+                <TouchableOpacity className={`absolute right-2 flex h-auto items-center justify-center px-2 w-28 flex-row rounded-md ${currentStyles.bg_6}`}
                     onPress={() => { handleBackground(); handleBackgroundPlayButton(); }}
                 >
                     <View className='flex justify-center items-center'>
@@ -44,14 +47,14 @@ const BackgroundPlay = ({ handleBackgroundPlayButton, ...props }: SleepProps) =>
                                 (bacgroundOptions.map((timeOption, id) => {
                                     return (
                                         <TouchableOpacity key={id} onPress={() => { setSelectedBackgroundPlay(timeOption); setShowBackgroundPlayOptions(false) }} className='flex flex-row items-center justify-start py-2 w-full'>
-                                            <Text>{timeOption}</Text>
+                                            <Text className={`${currentStyles.tx_white}`}>{timeOption}</Text>
                                         </TouchableOpacity>
                                     )
                                 })) :
                                 (
                                     <View className='flex flex-row items-center justify-between w-full py-2'>
-                                        <Text className=''>{selectedBackgroundPlay}</Text>
-                                        <DownArrow fill='black' width={14} height={14} />
+                                        <Text className={`${currentStyles.tx_white}`}>{selectedBackgroundPlay}</Text>
+                                        <DownArrow fill={currentStyles.svg_white} width={14} height={14} />
                                     </View>
                                 )
                         }
