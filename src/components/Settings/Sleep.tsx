55a0084a6931019ce4cc7de8bc65de73b2482e72
Ishara <isharamadushankab@gmail.com>
2024-11-02 08:40:56 +0530

    update Sleep.tsx

--- src/components/Settings/Sleep.tsx
+++ src/components/Settings/Sleep.tsx
@@ -10,7 +10,6 @@ type SleepProps = {
 
 const Sleep = ({ handleSleepButton, ...props }: SleepProps) => {
     const [selectedTime, setSelectedTime] = useState("Off");
-    const buttonRef = useRef(null);
 
 
     const { theme,showTimeOptions, setShowTimeOptions } = useGlobalContext();
@@ -32,25 +31,24 @@ const Sleep = ({ handleSleepButton, ...props }: SleepProps) => {
                 <Timer str={currentStyles.svg_1} />
                 <Text className={`${currentStyles.tx_1} text-xs font-semibold`}>Sleep Timer</Text>
             </View>
-            <View className='flex flex-row items-center h-full gap-1 relative'>
-                <TouchableOpacity className={`absolute right-1 flex h-auto items-center justify-center px-2 w-28 flex-row rounded-md ${currentStyles.bg_6}`}
-                ref={buttonRef}
+            <View className='flex flex-row items-center h-full relative'>
+                <TouchableOpacity className={`absolute right-1 flex h-auto items-center justify-center w-28 flex-row rounded-md ${currentStyles.bg_6}`}
                     onPress={() => { handleSleepTimer(); handleSleepButton(); }}
                 >
-                    <View className='flex justify-center items-center w-full'>
+                    <View className='flex w-full items-center'>
 
 
                         {
                             showTimeOptions ?
                                 (timeOptions.map((timeOption, id) => {
                                     return (
-                                        <TouchableOpacity key={id} onPress={() => { setSelectedTime(timeOption); setShowTimeOptions(false) }} className='flex flex-row items-center justify-center py-1 w-full'>
+                                        <TouchableOpacity key={id} onPress={() => { setSelectedTime(timeOption); setShowTimeOptions(false) }} className='flex-1 items-center py-1 w-full'>
                                             <Text className={`text-xs ${currentStyles.tx_white}`}>{timeOption} {timeOption === "Off" ? "" : "Minutes"}</Text>
                                         </TouchableOpacity>
                                     )
                                 })) :
                                 (
-                                    <View className='flex flex-row items-center justify-between w-full py-1'>
+                                    <View className='flex flex-row items-center justify-around w-full py-1'>
                                         <Text className={`${currentStyles.tx_white} text-xs `}>{selectedTime} {selectedTime === "Off" ? "" : "Minutes"} </Text>
                                         <DownArrow fill={currentStyles.svg_white} width={14} height={14} />
                                     </View>
