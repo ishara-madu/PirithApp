26645c6dcc05832ef50466f3f68e9b3fa799bb75
Ishara <isharamadushankab@gmail.com>
2025-01-10 06:57:17 +0530

    update 8 files and delete 1 file

--- babel.config.js
+++ babel.config.js
@@ -3,15 +3,7 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      "nativewind/babel",
-      ["module:react-native-dotenv", {
-        "moduleName": "@env",
-        "path": ".env",
-        "blacklist": null,
-        "whitelist": null,
-        "safe": false,
-        "allowUndefined": true
-      }]
+      "nativewind/babel"
     ]
   };
 };
--- firebaseConfig.tsx
+++ firebaseConfig.tsx
@@ -1,15 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import {
-  FIREBASE_API_KEY,
-  FIREBASE_AUTH_DOMAIN,
-  FIREBASE_PROJECT_ID,
-  FIREBASE_STORAGE_BUCKET,
-  FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_APP_ID,
-  FIREBASE_MEASUREMENT_ID,
-} from "@env";
 
+var FIREBASE_API_KEY="AIzaSyBt0cgSTJkAmmIbsi6g3FPNKK7OljETZk0"
+var FIREBASE_AUTH_DOMAIN="pirith-6fb06.firebaseapp.com"
+var FIREBASE_PROJECT_ID="pirith-6fb06"
+var FIREBASE_STORAGE_BUCKET="pirith-6fb06.firebasestorage.app"
+var FIREBASE_MESSAGING_SENDER_ID="799214750008"
+var FIREBASE_APP_ID="1:799214750008:web:98e9abb50f8158e2a199cd"
+var FIREBASE_MEASUREMENT_ID="G-8RGQ69DG09"
 // Your Firebase configuration
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
